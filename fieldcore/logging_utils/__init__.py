e838e3486f978f0d9e51315e570aeedce1f48709
andriusb82 <andrius.boznis@gmail.com>
2026-06-02 22:39:17 +0300

    Expose config based logging setup

--- fieldcore/logging_utils/__init__.py
+++ fieldcore/logging_utils/__init__.py
@@ -1,3 +1,13 @@
-from fieldcore.logging_utils.manager import get_logger, setup_logging
+from fieldcore.logging_utils.manager import (
+    LoggingManager,
+    get_logger,
+    setup_logging,
+    setup_logging_from_config,
+)
 
-__all__ = ["get_logger", "setup_logging"]
+__all__ = [
+    "LoggingManager",
+    "get_logger",
+    "setup_logging",
+    "setup_logging_from_config",
+]
