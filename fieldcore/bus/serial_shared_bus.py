35caf1fe6a0f68ba0d4568b8470cdd86771d49e3
andriusb82 <andrius.boznis@gmail.com>
2026-06-09 23:52:12 +0300

    Implement thread-safe execution in SerialSharedBus
    Add thread safety to SerialSharedBus with a lock.

--- fieldcore/bus/serial_shared_bus.py
+++ fieldcore/bus/serial_shared_bus.py
@@ -1,5 +1,15 @@
+from threading import Lock
+
 from fieldcore.bus.simple_bus import SimpleBus
+from fieldcore.transaction.result import TransactionResult
+from fieldcore.transaction.transaction import Transaction
 
 
 class SerialSharedBus(SimpleBus):
-    pass
+    def __init__(self, transport, protocol) -> None:
+        super().__init__(transport, protocol)
+        self._lock = Lock()
+
+    def execute(self, transaction: Transaction) -> TransactionResult:
+        with self._lock:
+            return super().execute(transaction)
